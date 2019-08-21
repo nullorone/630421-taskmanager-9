a04c87fa36a4c39e2fcad8f9095e7695a96a2840
nullorone <sy.v@mail.ru>
2019-08-21 21:22:21 +0300

    Избавься от модуля proxyMain

--- src/components/proxyMain.js
+++
@@ -1,9 +0,0 @@
-import {getSearchMarkup} from "./search";
-import {getFilterMarkup} from "./filter";
-import {getBoardTasksMarkup} from "./board";
-
-
-const getCombineMainMarkup = () => getSearchMarkup() + getFilterMarkup() + getBoardTasksMarkup();
-
-export {getCombineMainMarkup};
-
--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import {getButtonControlMarkup} from "./components/buttonControl";
-import {getCombineMainMarkup} from "./components/proxyMain";
 import {renderComponent} from "./utils/util";
+import {getSearchMarkup} from "./components/search";
+import {getFilterMarkup} from "./components/filter";
+import {getBoardTasksMarkup} from "./components/board";
 
 const main = document.querySelector(`.main`);
 const mainControl = document.querySelector(`.main__control`);
 
 const renderLayout = () => {
   renderComponent(mainControl, getButtonControlMarkup());
-  renderComponent(main, getCombineMainMarkup());
+  renderComponent(main, (getSearchMarkup() + getFilterMarkup() + getBoardTasksMarkup()));
 };
 
 renderLayout();
